25c6c33f5d9ffea0f95646b9815fb6b945d88fad
Aravind Roy <aravindroy1709@gmail.com>
2022-10-19 13:26:50 +0530

    Index Page Created and rendered

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app =express()
+const PORT = process.env.PORT | 3000
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs")
+})
+
+app.listen(PORT ,()=>{console.log("Server is up and running")})
